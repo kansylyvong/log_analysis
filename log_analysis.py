782cba5706a012029a75d17ea71adfb36317901e
vagrant <vagrant@vagrant.vm>
2018-11-01 19:41:37 +0000

    added function to calculate most popular authors

--- log_analysis.py
+++ log_analysis.py
@@ -11,4 +11,16 @@ def get_popular_articles():
     print(data)
     db.close()
 
+def get_popular_authors():
+    db = psycopg2.connect("dbname=news")
+
+    c = db.cursor()
+
+    c.execute("select authors.name, dt.views from (select substring(path, 10) as title, count(*) as views from log where path != '/' and status = '200 OK' group by 1 order by views desc) as dt join articles on articles.slug = dt.title join authors on articles.author = authors.id order by dt.views desc;")
+
+    data = c.fetchall()
+    print(data)
+    db.close()
+
 get_popular_articles()
+get_popular_authors()
