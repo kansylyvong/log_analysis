031558715a1dc4e4db5efc8af403f4e1dd756508
vagrant <vagrant@vagrant.vm>
2018-11-02 14:48:40 +0000

    Added blank README.md file

--- log_analysis.py
+++ log_analysis.py
@@ -1,36 +1,74 @@
 import psycopg2
 import datetime
 
+
 def get_popular_articles():
+
     db = psycopg2.connect("dbname=news")
 
     c = db.cursor()
 
-    c.execute("select articles.title, dt.views from (select substring(path, 10) as title, count(*) as views from log where path != '/' and status = '200 OK' group by 1 order by views desc limit 3 ) as dt join articles on articles.slug = dt.title order by dt.views desc;")
+    c.execute("""select articles.title, dt.views
+                 from (
+                 select substring(path, 10) as title, count(*) as views
+                 from log where path != '/' and status = '200 OK'
+                 group by 1 order by views desc limit 3 ) as dt
+                 join articles on
+                 articles.slug = dt.title
+                 order by dt.views desc;""")
 
     data = c.fetchall()
     for datum in data:
         print("\"%s\" -- %s views" % (datum[0], datum[1]))
     db.close()
+
 
 def get_popular_authors():
+
     db = psycopg2.connect("dbname=news")
 
     c = db.cursor()
 
-    c.execute("select authors.name, sum(dt.views) as views from ( select substring(path, 10) as title, count(*) as views from log where path != '/' and status = '200 OK' group by 1 order by views desc) as dt join articles on articles.slug = dt.title join authors on articles.author = authors.id group by authors.name order by views desc;")
+    c.execute("""select authors.name, sum(dt.views) as views
+                 from (
+                 select substring(path, 10) as title, count(*) as views
+                 from log where path != '/' and status = '200 OK'
+                 group by 1
+                 order by views desc) as dt
+                 join articles on
+                 articles.slug = dt.title
+                 join authors on
+                 articles.author = authors.id
+                 group by authors.name
+                 order by views desc;""")
 
     data = c.fetchall()
     for datum in data:
         print("\"%s\" -- %s views" % (datum[0], datum[1]))
     db.close()
 
+
 def get_errors_above_one():
+
     db = psycopg2.connect("dbname=news")
 
     c = db.cursor()
 
-    c.execute("select day, to_char(percentageOfErrors, '999D9%') as percentOfErrors  from ( select day, round( cast( float8 ((numberOfErrors/totalHits::float) * 100) as numeric), 1) as percentageOfErrors from ( select date(time) as day, sum(case when status = '404 NOT FOUND' then 1 else 0 end) as numberOfErrors, count(*) as totalHits from log group by day) as dt ) as ddt where percentageOfErrors > 1;")
+    c.execute("""select day,
+                 to_char(percentageOfErrors, '999D9%') as percentOfErrors
+                 from (
+                 select day,
+                 round( cast(float8((numberOfErrors/totalHits::float) * 100)
+                 as numeric), 1)
+                 as percentageOfErrors
+                 from (
+                 select date(time) as day,
+                 sum(case when status = '404 NOT FOUND' then 1 else 0 end)
+                 as numberOfErrors,
+                 count(*) as totalHits
+                 from log
+                 group by day) as dt ) as ddt
+                 where percentageOfErrors > 1;""")
 
     data = c.fetchall()
     for datum in data:
