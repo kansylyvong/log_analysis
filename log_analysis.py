7e10a5718d00c421f9d58f53a50abb1b1662a786
vagrant <vagrant@vagrant.vm>
2018-11-03 18:58:17 +0000

    moved limit to outer clause in pop articles query

--- log_analysis.py
+++ log_analysis.py
@@ -14,10 +14,11 @@ def get_popular_articles():
                  from (
                  select substring(path, 10) as title, count(*) as views
                  from log where path != '/' and status = '200 OK'
-                 group by 1 order by views desc limit 3 ) as dt
+                 group by 1 order by views desc) as dt
                  join articles on
                  articles.slug = dt.title
-                 order by dt.views desc;""")
+                 order by dt.views desc
+                 limit 3;""")
 
     data = c.fetchall()
     print("\nPrinting three most popular articles of all time\n")
